c15fa1e42e98e85ebb9045ff84e4fe6b0592ceab
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-04-03 11:42:40 -0500

    Fixing compiler issues in main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "monty.h"
-void push(stack_t **stack, unsigned int line_number)
-void pall(stack_t **stack, unsigned int line_number)
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
 /**
 *main - entry point
 *@argc: argument count
