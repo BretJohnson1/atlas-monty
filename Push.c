b95df948957337758e85dce10e68e00eb7fce047
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-04-03 11:28:23 -0500

    Created Push.c And Pall.c and renamed push_pall.c to main.c

---
+++ Push.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+*push - pushes an element to the stack
+*@stack: pointer to the stack
+*@line_number: line number
+*/
+void push(stack_t **stack, unsigned int line_number)
+{
+    stack_t *new_node;
+
+    new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->n = atoi(strtok(NULL, " \n\t"));
+    new_node->prev = NULL;
+    new_node->next = *stack;
+
+    if (*stack != NULL)
+    (*stack)->prev = new_node;
+    *stack = new_node;
+}
