6e97927fa070e9fa6747e074b6eb71530bca5ecb
BretJohnson1 <155486952+BretJohnson1@users.noreply.github.com>
2024-04-03 12:49:43 -0500

    Done with main.c

--- main.c
+++ main.c
@@ -98,7 +98,7 @@ void pushOp(stack_t **stack, unsigned int line_number, char *pushNum)
 		(*stack)->n = atoi(pushNum);
 		if ((*stack)->n == 0 || (pushNum[0] != '-' && (*stack)->n == -1))
 		{
-			fprintf(stderr, "L6: usage: push integer\n", line_number);
+			fprintf(stderr, "L%d: usage: push integer\n", line_number);
 			free_stack(stack);
 			error();
 		}
